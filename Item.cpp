4d5bf1c93b616234cd885933f7b57c1b1612f3a9
MatheusFarnese <95251433+MatheusFarnese@users.noreply.github.com>
2023-11-07 00:32:23 -0300

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,13 +1,3 @@
-/**
-* Engenharia de Software Moderna - Testes  (Cap. 8)
-* Prof. Marco Tulio Valente
-* 
-* Exemplo simples de teste (ShoppingCart)
-*
-*/
-
-/* Itens que serão armazenados no carrinho de compras */
-
 #include <iostream>
 #include <string>
 using namespace std;
